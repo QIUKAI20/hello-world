896a6a4eb6cf715d4cbb52cbae0cd034d520a4c4
QIUKAI20 <550761071@163.COM>
2017-06-22 13:03:44 +0800

    Create hello-world.py

---
+++ hello-world.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+# Author: YY-互娱事业部-张秋凯
+# YY: 909013219  QQ: 550761071
+# Date : 2017/06/22
+# 说明 ：hello wold
+
+from hashlib import md5
+
+def CheckStringMd5(args):
+    m = md5()
+    m.update(args)
+    str_md5 = m.hexdigest()
+    return str_md5
+    
+if __name__ == '__main__':
+    #doing is this...
+    #print CheckStringMd5("123456")
+    
